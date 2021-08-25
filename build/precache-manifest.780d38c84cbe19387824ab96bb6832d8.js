03902dc8f8a355c44d3d6297809e6ac5c42c5c84
Kartik Jha <kartik.n.jha@gmail.com>
2021-08-25 18:52:29 +0530

    fix: ts compiler updated

---
+++ build/precache-manifest.780d38c84cbe19387824ab96bb6832d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e0e618d6af05a6c58601fe730cc538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929ec54d34dfe2e00fed",
+    "url": "/static/css/main.d9f99577.chunk.css"
+  },
+  {
+    "revision": "d55be62a4b3974207f32",
+    "url": "/static/js/2.3ec0013f.chunk.js"
+  },
+  {
+    "revision": "929ec54d34dfe2e00fed",
+    "url": "/static/js/main.f7f2dcc2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
