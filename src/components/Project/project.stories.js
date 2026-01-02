1c314f220c2e8c13806ac52d829277666abb49d5
Kartik Jha <kartik.n.jha@gmail.com>
2026-01-02 17:12:09 +0530

    feat: removed unused files

--- src/components/Project/project.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-import Project from ".";
-
-storiesOf("Project", module).add("display message", () => (
-  <Project message="Hello World!" />
-));
--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-    GITHUB_API_BASE: "https://api.github.com/users/",
-    GITHUB_RAW_CONTENT_API_BASE: "https://raw.githubusercontent.com/"
-};
-export default config;
